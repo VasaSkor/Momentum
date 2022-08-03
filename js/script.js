8e7bb668216187e9f82f0610007443a9f46210b7
VasaSkor <dzho.ker.700@gmail.com>
2022-08-03 10:28:30 +0300

    feat: added the function of correctly displaying the time without refreshing the page in the format 00:00:00 to script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function showTime() {
+    const time = document.querySelector('.time')
+    const date = new Date()
+    const currentTime = date.toLocaleTimeString()
+    time.textContent = currentTime
+    setTimeout(showTime, 1000);
+    return time
+  }
+  showTime();
