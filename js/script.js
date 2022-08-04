24ac2dbff1e23ed14dd0808fb5bf64593c31d1ed
Vas Vasa <dzho.ker.700@gmail.com>
2022-08-04 09:00:15 +0300

    feat: added a function that creates the browsers local storage after the user enters their name to script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const bodyBackground = document.querySelector('body')
+bodyBackground.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/18.jpg')";
 function showTime() {
     const time = document.querySelector('.time')
     const date = new Date()
@@ -15,5 +17,40 @@ function showDate(){
   setTimeout(showDate, 1000);
   return date_block
 }
+
+function showGreetings(){
+  const greetings = document.querySelector('.greeting')
+  const arrGreetings = ['morning', 'afternoon', 'evening', 'night']
+  const date = new Date()
+  const GetHours = date.getHours()
+  setTimeout(showGreetings, 1000);
+  if(GetHours >= 6 && GetHours <= 11){
+   return greetings.textContent = `Good ${arrGreetings[0]}`
+  }
+  else if(GetHours >= 12 && GetHours <= 17){
+    return greetings.textContent = `Good ${arrGreetings[1]}`
+  }
+  else if(GetHours >= 18 && GetHours <= 23){
+    return greetings.textContent = `Good ${arrGreetings[2]}`
+  }
+  else if(GetHours >= 0 && GetHours <= 5){
+    return greetings.textContent = `Good ${arrGreetings[3]}`
+  }
+}
+
+function setLocalStorage() {
+  const name = document.querySelector('.name')
+  localStorage.setItem('name', name.value);
+}
+function getLocalStorage() {
+  const name = document.querySelector('.name')
+  if(localStorage.getItem('name')) {
+    name.value = localStorage.getItem('name');
+  }
+}
+
+  window.addEventListener('beforeunload', setLocalStorage)
+  window.addEventListener('load', getLocalStorage)
   showTime()
   showDate()
+  showGreetings()
