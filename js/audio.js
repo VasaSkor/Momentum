3ffd8bd87df5f8be2b416e8e7ed7d264ada829df
VasaSkor <dzho.ker.700@gmail.com>
2022-08-15 19:24:00 +0300

    feat: add new music file and objects

--- js/audio.js
+++ js/audio.js
@@ -5,9 +5,19 @@ const playList = [
       duration: '03:11'
     },  
     {      
-      title: 'River Flows In You',
-      src: '/assets/sounds/River Flows In You.mp3',
-      duration: '03:50'
-    }
+      title: 'Дерево',
+      src: '/assets/sounds/Victor Tsoi - Tree.mp3',
+      duration: '01:43'
+    },
+    {      
+      title: 'Два друга и разбойник',
+      src: '/assets/sounds/Two friends and a robber.mp3',
+      duration: '02:10'
+    },
+    {      
+      title: 'В пещере горного короля',
+      src: '/assets/sounds/v-peschere-gornogo-korolya.mp3',
+      duration: '04:07'
+    },
   ]
   export default playList;
