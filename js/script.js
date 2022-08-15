059bfda021ad662b0059d656c363f5936011c0ed
VasaSkor <dzho.ker.700@gmail.com>
2022-08-15 11:28:50 +0300

    feat: Added progress bar for audio player

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 import i18next from 'https://deno.land/x/i18next/index.js'
+import playList from './playList.js';
+console.log(playList);
 let randomNum = Math.floor(Math.random() * 20 + 1).toString().padStart(2, '0')
+let randomNumAPI = Math.floor(Math.random() * 100 + 1).toString().padStart(2, '0')
 let slideNext = document.querySelector('.slide-next')
 let slidePreview = document.querySelector('.slide-prev')
 const weatherIcon = document.querySelector('.weather-icon')
@@ -21,88 +24,68 @@ const progress = document.querySelector('.progress')
 const trackNames = ["Ennio Morricone", "Aqua Caelestis", "River Flows In You", "Summer Wind"]
 
 
-let isPlayed = false
-let currentTrack = 0
-let playlist = trackNames.map((name) =>
-    new Audio(
-        `https://github.com/VasaSkor/Momentum/blob/momentum/assets/sounds/${
-            name.replaceAll(' ', '%20')
-        }.mp3?raw=true`
-    ))
-let currentAudio = playlist[0]
 
+const audioPlayer = document.querySelector(".player");
+const audio = new Audio(
+  "https://ia800905.us.archive.org/19/items/FREE_background_music_dhalius/FaceBangSonic.mp3"
+);
+console.dir(audio);
+audio.addEventListener(
+  "loadeddata",
+  () => {
+    audioPlayer.querySelector(".times .length").textContent = getTimeCodeFromNum(
+      audio.duration
+    );
+    audio.volume = .75;
+  },
+  false
+);
 
-window.addEventListener('load', () => {
-    playlist.forEach((track, index) => {
-        track.load()
+function getTimeCodeFromNum(num) {
+  let seconds = parseInt(num);
+  let minutes = parseInt(seconds / 60);
+  seconds -= minutes * 60;
+  const hours = parseInt(minutes / 60);
+  minutes -= hours * 60;
 
-        track.addEventListener('canplaythrough', () => {
-            const track = document.createElement('li')
-            track.textContent = trackNames[index]
-
-            if (index == currentTrack)
-                track.classList = 'play-item item-active'
-            else
-                track.classList = 'play-item'
-
-
-            playlistTable.appendChild(track)
-        })
-    })
-})
-
-nextPlayerButton.addEventListener('click', () => {
-    currentTrack++
-    
-    if (currentTrack > playlist.length - 1)
-        currentTrack = 0
-
-    markSelectedTrack()
-    updatePlayerTrack()
-})
-
-previousPlayerButton.addEventListener('click', () => {
-    currentTrack--
-    
-    if (currentTrack < 0)
-        currentTrack = playlist.length - 1
-
-    markSelectedTrack()
-    updatePlayerTrack()
-})
-
-playButton.addEventListener('click', () => {
-    if (isPlayed){
-        currentAudio.pause()
-        playButton.classList = 'play player-icon'
-    }
-    else{
-        currentAudio.play()
-        playButton.classList = 'play player-icon pause'
-    }
-        
-
-    isPlayed = !isPlayed
-})
-
-function updatePlayerTrack() {
-    if (isPlayed === true) {
-        currentAudio.pause()
-        currentAudio = playlist[currentTrack]
-        currentAudio.play()
-    }
-    else {
-        currentAudio = playlist[currentTrack]
-    }
+  if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+  return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+    seconds % 60
+  ).padStart(2, 0)}`;
 }
 
-function markSelectedTrack() {
-    for (let i = 0; i < playlistTable.childElementCount; i++) {
-        playlistTable.children[i].classList = 'play-item'
-    }
+const timeline = audioPlayer.querySelector(".duration-player");
+timeline.addEventListener("click", e => {
+  const timelineWidth = window.getComputedStyle(timeline).width;
+  const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
+  audio.currentTime = timeToSeek;
+}, false);
 
-    playlistTable.children[currentTrack].classList = 'play-item item-active'
-}
+setInterval(() => {
+  const progressBar = audioPlayer.querySelector(".progress");
+  progressBar.style.width = audio.currentTime / audio.duration * 100 + "%";
+  audioPlayer.querySelector(".times .current").textContent = getTimeCodeFromNum(
+    audio.currentTime
+  );
+}, 500);
+
+const playBtn = audioPlayer.querySelector(".play");
+playBtn.addEventListener(
+  "click",
+  () => {
+    if (audio.paused) {
+      playBtn.classList.remove("play");
+      playBtn.classList.add("pause");
+      audio.play();
+    } else {
+      playBtn.classList.remove("pause");
+      playBtn.classList.add("play");
+      audio.pause();
+    }
+  },
+  false
+);
+
 
 function showTime() {
     const time = document.querySelector('.time')
@@ -184,6 +167,29 @@ else if(GetHours >= 0 && GetHours <= 5){
 }
 }
 
+async function getLinkToImage() {
+  const url = 'https://api.unsplash.com/photos/random?query=morning&client_id=e2077ad31a806c894c460aec8f81bc2af4d09c4f8104ae3177bb809faf0eac17';
+  const res = await fetch(url);
+  const data = await res.json();
+  const image = new Image
+  image.src = data.urls.regular
+  image.onload = () =>
+    document.querySelector('body').style.backgroundImage = `url(${image.src})`
+  console.log(data.urls.regular)
+ }
+
+async function getLinkToImageFetch() {
+  let index = randomNumAPI.toString().padStart(2, '0')
+  const url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=edf799313c53f683728b161f72fedbf4&tags=nature&extras=url_l&format=json&nojsoncallback=1';
+  const res = await fetch(url);
+  const data = await res.json();
+  const image = new Image
+  image.src = data.photos.photo[`${index}`].url_l
+  image.onload = () =>
+    document.querySelector('body').style.backgroundImage = `url(${image.src})`
+    console.log(data.photos.photo[0].url_l)
+}
+
 const getSlideNext = () =>{
   randomNum++
   if (randomNum > 20)
@@ -195,6 +201,13 @@ const getSlidePrev = () =>{
   if(randomNum < 1)
      randomNum = 20
      getRandomBackgroundImage()
+}
+
+const getSlideNextRandom = () =>{
+  getLinkToImageFetch()
+}
+const getSlidePrevRandom= () =>{
+  getLinkToImageFetch()
 }
 
 cityInput.onkeypress = async (event) => {
@@ -238,13 +251,13 @@ async function getWeather() {
 function updateQuote() {
   const quote = quotes[Math.floor(Math.random() * quotes.length)]
 
-  quoteText.textContent = `"${quote['text']}"`
+  quoteText.textContent = `"${quote['quote']}"`
   quoteAuthor.textContent = quote['author']
 }
 
 
 async function getQuotes() {
-  const response = await fetch('https://raw.githubusercontent.com/rolling-scopes-school/file-storage/random-jokes/quotes.json')
+  const response = await fetch(`https://raw.githubusercontent.com/VasaSkor/Momentum/momentum/js/quotes${i18next.t('quotesLang')}.json`)
   
 
   if (response.ok){
@@ -283,6 +296,7 @@ quoteChangeButton.addEventListener('click', updateQuote)
   showGreetings()
   getWeather()
 
+
   window.addEventListener('DOMContentLoaded', async () =>{
     await i18next.init({
       lng: navigator.language,
@@ -303,6 +317,7 @@ quoteChangeButton.addEventListener('click', updateQuote)
             "afternoon": 'afternoon',
             "evening": 'evening',
             "night": 'night',
+            "quotesLang": 'EN'
           }
         },
         ru:{
@@ -320,6 +335,7 @@ quoteChangeButton.addEventListener('click', updateQuote)
             "afternoon": 'дня',
             "evening": 'вечера',
             "night": 'ночи',
+            "quotesLang": 'RU'
           }
         }
       }
